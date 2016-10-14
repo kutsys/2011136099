afcbd3bb062b42ae50c20a2f317ea29fcd23f920
kutsys <gkgkgy@koreatech.ac.kr>
2016-10-14 11:27:57 +0900

    Lab-3: LowCopy, HighCopy (Dot, * 출력 알고리즘 개선 및 출력 문제 개선)

--- 161014/HighCopy.c
+++ 161014/HighCopy.c
@@ -101,6 +101,7 @@ int main(int argc, char* argv[]) {
         if (block_count >= file_size/10) {
             block_count = 1;
             printf("*");
+            fflush(stdout);
         }
     }
     
